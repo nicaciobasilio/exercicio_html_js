abb347b5492d5e37d3c88a0526324289ede5ba31
Nicacio Basilio <nicacioufrn@gmail.com>
2022-11-02 00:06:37 -0300

    correcao

--- main.js
+++ main.js
@@ -2,25 +2,29 @@ const form = document.getElementById("form-envio")
 const quantidadeVendida = document.getElementById("quantidade-vendida")
 const restanteEstoque = document.getElementById("restante-estoque")
 
+
 form.addEventListener('submit', function(e){
     e.preventDefault();
     
-    const quantidadeVendida = document.getElementById("#quantidade-vendida")
-    const restanteEstoque = document.getElementById("#restante-estoque")
-    const mensagemSucesso = `Foi vendida a quantidade de: <b>${quantidadeVendida}</b> e a quantidade restante no estoque é de: <b>${restanteEstoque}</b>`
-    const mensagemNegativa = `O formulário não pode ser enviado pois a quantidade restante no estoque é de: <b>${restanteEstoque}</b> sendo inferior a quantidade vendida de: <b>${quantidadeVendida}</b>`
+    const quantidadeVendida = document.getElementById("quantidade-vendida")
+    const restanteEstoque = document.getElementById("restante-estoque")
+    const mensagemSucesso = `Foi vendida a quantidade de: <b>${quantidadeVendida.value}</b> e a quantidade restante no estoque é de: <b>${restanteEstoque.value}</b>`
+    const mensagemNegativa = `O formulário não pode ser enviado pois a quantidade restante no estoque é de: <b>${restanteEstoque.value}</b> sendo inferior a quantidade vendida de: <b>${quantidadeVendida.value}</b>`
     const msgNegativa = document.querySelector(".nao-enviou")
     const msgPositiva = document.querySelector(".envio-sucedido")
 
-    formEValido = restanteEstoque >= quantidadeVendida
+    formEValido = restanteEstoque.value >= quantidadeVendida.value
     if (formEValido) {
         document.querySelector(".envio-sucedido").innerHTML = mensagemSucesso
         msgPositiva.style.display = "block"
-        msgPositiva.style.display = "none"
+        msgNegativa.style.display = "none"
+
+        quantidadeVendida.value = ''
+        restanteEstoque.value = ''
     } else {
         document.querySelector(".nao-enviou").innerHTML = mensagemNegativa
-        msgNegativa.style.displau = 'block'
+        msgNegativa.style.display = 'block'
         msgPositiva.style.display = "none"
-    }
+    } 
 }
 )
